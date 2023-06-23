193d9cc2cdc5436642c64e906d793a0a2bf09b25
edd-ie <ogendoeddie@gmail.com>
2023-06-23 11:31:41 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :no_record
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
+
+    private
+    def no_record
+        render json: {error: 'No record found'}, status: :not_found
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :no_record
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+    def no_record
+        render json: {error: 'No record found'}, status: :not_found
+    end
 end
